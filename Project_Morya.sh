c031fa49804f310faaafe0c132c80334bc4710c3
Anubhav Singh <anubhavsinghhacker@gmail.com>
2021-07-13 11:51:17 +0530

    Update Project_Morya.sh

--- Project_Morya.sh
+++ Project_Morya.sh
@@ -14,11 +14,11 @@ echo "
                           ░░░░░░                                                                     ░░░░░░            
                       ░░░░░░                                                               ░░░░░░                                          ░░░░░░                                                                       ░░░░░░             
 "
-echo "Please enter a domain!"
+printf "Please enter a domain!"
 read DOMAIN
 
-echo "Domain provide to us: $DOMAIN"
-echo "Creating directory ${DOMAIN}_recon"
+printf "Domain provide to us: $DOMAIN\n"
+printf "Creating directory ${DOMAIN}_recon\n"
 
 Directory="${DOMAIN}_recon"
 mkdir $Directory
@@ -26,28 +26,28 @@ cd $Directory
 mkdir Subdomains
 cd Subdomains
 
-echo "---------Collecting DNS_Resolvers---------"
+printf "\n---------Collecting DNS_Resolvers---------\n"
 dnsvalidator -tL https://public-dns.info/nameservers.txt -threads 200 -o resolvers.txt
 
-echo "---------Starting Subdomain Enumeration---------\n"
+printf "---------Starting Subdomain Enumeration---------\n"
 
-echo "---------Running Subfinder---------"
+printf "---------Running Subfinder---------\n"
 subfinder -d $DOMAIN -all -config config.yaml -o allsubs1.txt
 
-echo "---------Running CRT.sh---------"
+printf "---------Running CRT.sh---------\n"
 curl "https://crt.sh/?q=$DOMAIN&output=json" | jq -r ".[] | .name_value" >>allsubs2.txt
 
-echo "---------Running AssetFinder---------"
+printf "---------Running AssetFinder---------\n"
 assetfinder —subs-only $DOMAIN >>allsubs3.txt
 
-echo "---------Running Finddomain---------"
+printf "---------Running Finddomain---------\n"
 findomain -o -t $DOMAIN --resolvers resolvers.txt --threads 100
 mv $DOMAIN.txt allsubs4.txt
 
-echo "---------Running sd-goo.sh (Domains from google search)---------"
+printf "---------Running sd-goo.sh (Domains from google search)---------\n"
 sd-goo.sh $DOMAIN | sort -u >>allsubs5.txt
 
-echo "---------Running Shodan to find---------"
+printf "---------Running Shodan to find---------\n"
 
 shodan_dorks=(ssl.cert.subject.cn:$DOMAIN hostname:$DOMAIN org:$DOMAIN)
 for i in "${shodan_dorks[@]}"; do
@@ -55,41 +55,41 @@ for i in "${shodan_dorks[@]}"; do
 	cat shodan_${DOMAIN}_ips.txt | awk -F ':' '{print $1}' | dnsx -ptr -resp-only >>allsubs6.txt
 done
 
-echo "---------Running Amass---------"
+printf "---------Running Amass---------\n"
 amass enum -passive -d $DOMAIN -config config.ini -o allsubs7.txt
 
-echo "---------Running gauplus---------"
+printf "---------Running gauplus---------\n"
 gauplus -t 15 -random-agent -subs $DOMAIN | unfurl -u domains | sort -u allsubs8.txt
 
-echo "---------Running Waybackurls---------"
+printf "---------Running Waybackurls---------\n"
 waybackurls $DOMAIN | unfurl -u domains | sort -u allsubs9.txt
 
-echo "---------Running Github-subdomains---------"
+printf "---------Running Github-subdomains---------\n"
 github-subdomains -d $DOMAIN -t tokens.txt -o allsubs10.txt
 
-echo "---------Running crobat---------"
+printf "---------Running crobat---------\n"
 crobat -s $DOMAIN >allsubs11.txt
 
-echo "---------Running tls.bufferover---------"
+printf "---------Running tls.bufferover---------\n"
 curl "https://tls.bufferover.run/dns?q=$DOMAIN" | jq -r .Results'[]' | cut -d ',' -f3 | grep -F "$DOMAIN" >>allsubs12.txt
 
-echo "---------Running dns.bufferover---------"
+printf "---------Running dns.bufferover---------\n"
 curl "https://dns.bufferover.run/dns?q=$DOMAIN" | jq -r .FDNS_A'[]',.RDNS'[]' | cut -d ',' -f2 | grep -F "$DOMAIN" >>allsubs13.txt
 
-echo "---------Running Puredns---------"
+printf "---------Running Puredns---------\n"
 dnsvalidator -tL https://public-dns.info/nameservers.txt -threads 200 -o resolvers.txt
 puredns bruteforce best-dns-wordlist.txt $DOMAIN -r resolvers.txt -w allsubs14.txt --wildcard-batch 1000000
 
-echo "---------Sorting Colleted Subdomains---------"
+printf "---------Sorting Colleted Subdomains---------\n"
 sort allsubs*.txt | uniq -u >subdomains.txt
 
-echo "---------Running DNSCewl---------"
+printf "---------Running DNSCewl---------\n"
 DNScewl --tL subdomains.txt -p permutations_list.txt --level=0 --subs --no-color | tail -n +14 >permutations.txt
 dnsvalidator -tL https://public-dns.info/nameservers.txt -threads 200 -o resolvers.txt
 puredns resolve permutations.txt -r resolvers.txt -w allsubs15.txt
 rm permutations.txt
 
-echo "---------Scraping Subdomains from JS/Source code---------"
+printf "---------Scraping Subdomains from JS/Source code---------\n"
 cat subdomains.txt | httpx -random-agent -retries 2 -no-color -o probed_tmp_scrap.txt
 gospider -S probed_tmp_scrap.txt --js -t 50 -d 3 --sitemap --robots -w -r >gospider.txt
 sed -i '/^.\{2048\}./d' gospider.txt
@@ -97,7 +97,7 @@ cat gospider.txt | grep -Eo 'https?://[^ ]+' | sed 's/]$//' | unfurl -u domains 
 rm gospider.txt
 sort subdomains.txt allsub16.txt | uniq -u >subdomains.txt
 
-echo "---------Probing on common ports:--------"
+echo "---------Probing on common ports:--------\n"
 COMMON_PORTS_WEB="81,300,591,593,832,981,1010,1311,1099,2082,2095,2096,2480,3000,3128,3333,4243,4567,4711,4712,4993,5000,5104,5108,5280,5281,5601,5800,6543,7000,7001,7396,7474,8000,8001,8008,8014,8042,8060,8069,8080,8081,8083,8088,8090,8091,8095,8118,8123,8172,8181,8222,8243,8280,8281,8333,8337,8443,8500,8834,8880,8888,8983,9000,9001,9043,9060,9080,9090,9091,9200,9443,9502,9800,9981,10000,10250,11371,12443,15672,16080,17778,18091,18092,20720,32000,55440,55672"
 sudo unimap --fast-scan -f subdomains.txt --ports $COMMON_PORTS_WEB -q -k --url-output >unimap_commonweb.txt
 cat unimap_commonweb.txt | httpx -random-agent -status-code -silent -retries 2 -no-color | cut -d ' ' -f1 >>probed_common_ports.txt
