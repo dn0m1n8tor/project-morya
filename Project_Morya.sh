c9786bf51113db660495975a1f63c2f84f23cf92
Anubhav Singh <anubhavsinghhacker@gmail.com>
2021-07-13 22:16:03 +0530

    Update Project_Morya.sh

--- Project_Morya.sh
+++ Project_Morya.sh
@@ -32,7 +32,7 @@ dnsvalidator -tL https://public-dns.info/nameservers.txt -threads 500 -o resolve
 printf "Starting Subdomain Enumeration\n" | nofity
 
 printf "Running Subfinder\n" | nofity
-subfinder -d $DOMAIN -all --silent -config subfinder_config.yaml -o allsubs1.txt
+subfinder -d $DOMAIN -all --silent -config ../config_files/subfinder_config.yaml -o allsubs1.txt
 
 printf "Running CRT.sh\n" | nofity
 curl "https://crt.sh/?q=$DOMAIN&output=json" | jq -r ".[] | .name_value" >>allsubs2.txt
@@ -55,16 +55,16 @@ sd-goo.sh $DOMAIN | sort -u >>allsubs5.txt
 # done
 
 printf "Running Amassn" | nofity
-amass enum -passive -d $DOMAIN -config config.ini -o allsubs7.txt
+amass enum -passive -d $DOMAIN -config ../config_files/config.ini -o allsubs7.txt
 
 printf "Running gauplus\n" | nofity
-gauplus -t 15 -random-agent -subs $DOMAIN | unfurl -u domains | sort -u >>allsubs8.txt
+gauplus -t 5 -random-agent -subs $DOMAIN | unfurl -u domains | sort -u >>allsubs8.txt
 
 printf "Running Waybackurls\n" | nofity
 waybackurls $DOMAIN | unfurl -u domains | sort -u >>allsubs9.txt
 
 printf "Running Github-subdomains\n" | nofity
-github-subdomains -d $DOMAIN -t tokens.txt -o allsubs10.txt
+github-subdomains -d $DOMAIN -t ../config_files/tokens.txt -o allsubs10.txt
 
 printf "Running crobat\n" | nofity
 crobat -s $DOMAIN >>allsubs11.txt
@@ -81,11 +81,11 @@ puredns bruteforce best-dns-wordlist.txt $DOMAIN -r resolvers.txt -w allsubs14.t
 printf "Sorting Colleted Subdomains\n" | nofity
 sort allsubs*.txt | uniq -u >>subdomains.txt
 
-# printf "Running DNSCewl\n" | nofity
-# DNScewl --tL subdomains.txt -p permutations_list.txt --level=0 --subs --no-color | tail -n +14 >permutations.txt
-# printf "Running Puredns for resolving permutatuon subdomains\n"
-# puredns resolve permutations.txt -r resolvers.txt --wildcard-batch 1000000 -w allsubs15.txt
-# rm permutations.txt
+printf "Running DNSCewl\n" | nofity
+DNScewl --tL subdomains.txt -p permutations_list.txt --level=0 --subs --no-color | tail -n +14 >permutations.txt
+printf "Running Puredns for resolving permutatuon subdomains\n"
+puredns resolve permutations.txt -r resolvers.txt --wildcard-batch 1000000 -w allsubs15.txt
+rm permutations.txt
 
 printf "Scraping Subdomains from JS/Source code\n" | nofity
 cat subdomains.txt | httpx -random-agent -retries 2 -no-color -o probed_tmp_scrap.txt
@@ -97,6 +97,6 @@ sort subdomains.txt allsub16.txt | uniq -u >>subdomains.txt
 
 # echo "Probing on common ports \n" | nofity
 # COMMON_PORTS_WEB="81,300,591,593,832,981,1010,1311,1099,2082,2095,2096,2480,3000,3128,3333,4243,4567,4711,4712,4993,5000,5104,5108,5280,5281,5601,5800,6543,7000,7001,7396,7474,8000,8001,8008,8014,8042,8060,8069,8080,8081,8083,8088,8090,8091,8095,8118,8123,8172,8181,8222,8243,8280,8281,8333,8337,8443,8500,8834,8880,8888,8983,9000,9001,9043,9060,9080,9090,9091,9200,9443,9502,9800,9981,10000,10250,11371,12443,15672,16080,17778,18091,18092,20720,32000,55440,55672"
-# sudo unimap --fast-scan -f subdomains.txt --ports $COMMON_PORTS_WEB -q -k --url-output >unimap_commonweb.txt
+# unimap --fast-scan -f subdomains.txt --ports $COMMON_PORTS_WEB -q -k --url-output >unimap_commonweb.txt
 # cat unimap_commonweb.txt | httpx -random-agent -status-code -silent -retries 2 -no-color | cut -d ' ' -f1 >>probed_common_ports.txt
 # rm unimap_commonweb.txt
