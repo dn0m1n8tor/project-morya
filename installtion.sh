5984370dd95dea43142edd6f2d9c8d12ae4dd354
Anubhav Singh <anubhavsinghhacker@gmail.com>
2021-09-18 21:30:26 +0530

    Update installtion.sh

--- installtion.sh
+++ installtion.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #============================================================================================
-
 #Download the Go binary
 wget https://golang.org/dl/go1.15.10.linux-amd64.tar.gz
 
@@ -20,4 +19,122 @@ export PATH=$GOPATH/bin:$GOROOT/bin:$HOME/.local/bin:$PATH
 source ~/.bashrc 
 
 #==============================================================================================
+#Installing pip3
+
+sudo apt-get install python3-pip
+
+#==============================================================================================
+#Installing subfinder
+
+GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
+
+#==============================================================================================
+#Installing ctfr.py
+
+cd $HOME/tools/ && git clone https://github.com/UnaPibaGeek/ctfr.git
+cd ctfr
+pip3 install -r requirements.txt
+
+#==============================================================================================
+#Installing Assestfinder
+
+go get -u github.com/tomnomnom/assetfinder
+
+#==============================================================================================
+#Installing Findomain
+
+cd $HOME/tools/ && wget https://github.com/findomain/findomain/releases/latest/download/findomain-linux
+chmod +x findomain-linux
+mv findomain-linux /usr/local/bin/findomain
+
+#==============================================================================================
+#Installing sd-goo
+
+cd $HOME/tools/ && git clone https://github.com/darklotuskdb/sd-goo.git && cd sd-goo && chmod +x *.sh 
+
+#==============================================================================================
+#Installing shodan
+
+pip install -U --user shodan
+
+#==============================================================================================
+#Installing anew
+
+go get -u github.com/tomnomnom/anew
+
+#==============================================================================================
+#Installing amass
+
+snap refresh
+sudo snap install amass
+
+#==============================================================================================
+#Installing gauplus
+
+GO111MODULE=on go get -u -v github.com/bp0lr/gauplus
+
+#==============================================================================================
+#Installing waybackurls
+
+go get github.com/tomnomnom/waybackurls
+
+#==============================================================================================
+#Installing github-subdomains
+
+go get -u github.com/gwen001/github-subdomains
+
+#==============================================================================================
+#Installing Crobat
+
+go get github.com/cgboal/sonarsearch/crobat
+
+#==============================================================================================
+#Installing Puredns
+
+cd $HOME/tools/ && git clone https://github.com/blechschmidt/massdns.git
+cd massdns && make && make install
+
+GO111MODULE=on go get github.com/d3mondev/puredns/v2
+
+#==============================================================================================
+#Installing DNSCewl
+
+cd $HOME/tools/ && wget https://github.com/codingo/DNSCewl/raw/master/DNScewl
+chmod 755 DNScewl
+mv DNScewl /usr/local/bin/DNScewl
+
+#==============================================================================================
+#Installing dnsvalidator
+
+cd $HOME/tools/ && git clone https://github.com/vortexau/dnsvalidator.git
+cd dnsvalidator/
+python3 setup.py install
+
+#==============================================================================================
+#Installing httpx
+
+GO111MODULE=on go get -v github.com/projectdiscovery/httpx/cmd/httpx
+
+#==============================================================================================
+#Installing Gospider
+
+GO111MODULE=on go get -u github.com/jaeles-project/gospider
+
+#==============================================================================================
+#Installing Notify
+
+GO111MODULE=on go get -v github.com/projectdiscovery/notify/cmd/notify
+
+#==============================================================================================
+#Installing Unfurl
+
+go get -u github.com/tomnomnom/unfurl
+
+#==============================================================================================
+#Installing Unimap
+
+cd $HOME/tools/ && eval wget -N -c https://github.com/Edu4rdSHL/unimap/releases/download/0.4.0/unimap-linux
+mv unimap-linux /usr/local/bin/unimap
+
+printf "\n Tools are successfully Installed"
 
